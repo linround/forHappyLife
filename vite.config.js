63a18d36a9fad3185877f30c52c380ba2072d025
linyuan <yuanlincuc@gmail.com>
2024-08-29 14:28:22 +0800

    feature: 配置开发选项

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    host:true,// 设置可通过ip访问
+    open:true,// 自动打开浏览器
+  }
 })
